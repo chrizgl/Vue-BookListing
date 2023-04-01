8ae124d6e167cfe7a481a254dc268982696d770b
Christian Gierl <c.gierl@outlook.com>
2023-04-01 20:09:07 +0200

    first try modul 2

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,12 +1,25 @@
 <template>
     <div>
         <h1>{{ title }}</h1>
+        <ul>
+            <li>
+                {{ books.title }},
+                {{ books.author }}
+            </li>
+        </ul>
     </div>
 </template>
 <script>
     export default {
         name: 'BookList',
-        data() {
+        data(
+            books =
+            [
+                {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+                {title: 'American Gods', author: 'Neil Gaiman'},
+                {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},   
+            ]
+        ) {
             return {
                 title: 'All Books'
             }
@@ -16,5 +29,13 @@
 <style>
     h1, h2 {
         font-weight: normal;
+    }
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        display: block;
+        margin: 0 10px;
     }
 </style>

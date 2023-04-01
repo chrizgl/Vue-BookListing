b9aa8f44c761d14c91fe486571a571115932f21c
Christian Gierl <c.gierl@outlook.com>
2023-04-01 21:38:27 +0200

    modul 5 try 2

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,12 +1,19 @@
-<template></template>
+<template>
+    <form class="" action="#" method="post">
+    <input type="text" name="title" value="" placeholder="Book Title">
+    <input type="text" name="author" value="" placeholder="Book Author">
+    <button type="submit" name="button">Add Book</button>
+</form>
+</template>
+
 <script>
     export default {
         name: 'BookForm',
-        props: ['book'],
+        props: ['books'],
         data() {
             return {
-                title: '',
-                author: ''
+                bookTitle: '',
+                bookAuthor: ''
             }
         }
     }

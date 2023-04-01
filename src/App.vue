effb71877f48520b0a7cd5a4625950f0be2a1b94
Christian Gierl <c.gierl@outlook.com>
2023-04-01 19:47:21 +0200

    debugging modul 1 complete

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello-world></hello-world>
+    <book-list></book-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/Booklist.vue';
+import BookList from './components/BookList';
 
 export default {
   name: 'app',
--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -7,17 +7,14 @@
     export default {
         name: 'BookList',
         data() {
-            return   {
-                tilte: 'All Books',
-            };
-        },
-    };
+            return {
+                title: 'All Books'
+            }
+        }
+    }
 </script>
 <style>
-    h1 {
-        font-weight: normal;
-    }
-    h2 {
+    h1, h2 {
         font-weight: normal;
     }
 </style>

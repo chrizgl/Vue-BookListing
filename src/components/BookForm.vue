63dbd48e4fc2f3340475c984178438ffa2988913
Christian Gierl <c.gierl@outlook.com>
2023-04-01 21:33:07 +0200

    pre try

---
+++ src/components/BookForm.vue
@@ -0,0 +1,14 @@
+<template></template>
+<script>
+    export default {
+        name: 'BookForm',
+        props: ['book'],
+        data() {
+            return {
+                title: '',
+                author: ''
+            }
+        }
+    }
+</script>
+<style></style>

25e01954045cc4f16c0b4c7a296c65ada7cf9e8e
Christian Gierl <c.gierl@outlook.com>
2023-04-01 20:59:56 +0200

    solved and debugged

---
+++ src/components/BookItem.vue
@@ -0,0 +1,21 @@
+<template>
+    <ul>
+        <li v-for="book in books">{{ book.title }}: {{ book.author }}</li>
+    </ul>
+</template>
+<script>
+    export default {
+        name: 'BookItem',
+        props: ['book'],
+    }
+</script>
+<style>
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    display: block;
+    margin: 0 10px;
+}
+</style>
--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,9 +1,6 @@
 <template>
     <div>
         <h1>{{ title }}</h1>
-        <ul>
-            <li v-for = "book in books"> {{ book.title }}:{{ book.author }} </li>
-        </ul>
     </div>
 </template>
 <script>
@@ -26,13 +23,5 @@
 <style>
     h1, h2 {
         font-weight: normal;
-    }
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
-    li {
-        display: block;
-        margin: 0 10px;
     }
 </style>
